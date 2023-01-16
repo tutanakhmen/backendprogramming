451df006c22e0237cbadb9f2e3e0a7cab2544fca
Harlequin <90820020+tutanakhmen@users.noreply.github.com>
2023-01-16 10:09:21 +0700

    Delete Our Last Assignment/Project-UAS directory

--- Our Last Assignment/Project-UAS/ExpressJS1/ExpressJS-main/data/students.js
+++
@@ -1,7 +0,0 @@
-// TODO 1: Buat data students
-// code here
-const students = ["Ammar Ayyasy", "Dea Eka", "Restu Tami"];
-
-// TODO 2: export data students
-// code here
-module.exports = students;
--- Our Last Assignment/Project-UAS/Readme.md
+++
@@ -1,1 +0,0 @@
-### Project UAS menggunakan Express.JS
